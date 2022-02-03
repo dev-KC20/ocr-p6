641c165bd93ae512aa04f242e92625fa8f020c53
dev@KC <dev@kitconseil.fr>
2022-02-03 15:07:31 +0100

    chore: get list of movies

--- src/index.js
+++ src/index.js
@@ -1,67 +1,164 @@
+/**
+ * Represents a movie as  described in imdb database and to be shown if requested thru Modal.
+ * @constructor
+ * @param {number} id - The id of the movie within the API db..
+ * @param {uri} url - where the uri of the resource can be found.
+ * @param {string} title - The author of the movie.
+ * @param {number} imdb_score - The score collected by the site after visitor's votes.
+ * @param {uri} image_url - The image from the movie poster.
+ * @param {Array} genres - The genres the movie belongs to.
+ */
+
 class Movie {
-  constructor(id, url, title, original_title, year, date_published, duration, description, long_description,
-    avg_vote, imdb_score, votes, metascore, image_url, actors, directors, writers, genres, countries, languages, rated, company) {
+
+  constructor(id, url, image_url, title, genres, date_published, rated, imdb_score, directors, actors, duration, countries, description, worldwide_gross_income) {
     this.id = id;
     this.url = url;
-    this.title = title;
-    this.original_title = original_title;
-    this.year = year;
-    this.date_published = date_published;
-    this.duration = duration;
-    this.description = description;
-    this.long_description = long_description;
-    this.avg_vote = avg_vote;
-    this.imdb_score = imdb_score;
-    this.votes = votes;
-    this.metascore = metascore;
     this.image_url = image_url;
-    this.actors = actors;
-    this.directors = directors;
-    this.writers = writers;
+    this.title = title;
     this.genres = genres;
-    this.countries = countries;
-    this.languages = languages;
+    this.date_published = date_published;
     this.rated = rated;
-    this.company = company;
+    this.imdb_score = imdb_score;
+    this.directors = directors;
+    this.actors = actors;
+    this.duration = duration;
+    this.countries = countries;
+    this.description = description;
+    this.worldwide_gross_income = worldwide_gross_income;
+    // this.year = year;
+    // this.long_description = long_description;
+    // this.avg_vote = avg_vote;
+    // this.votes = votes;
+    // this.metascore = metascore;
+    // this.writers = writers;
+    // this.languages = languages;
+    // this.company = company;
   }
 
   async init(url) {
-    let response = await fetch(url);
-    if (!response.ok) { throw new Error('Fetch missed, pls check API server'); }
-    let data = await response.json();
+    let responseFactory = await fetch(url);
+    if (!responseFactory.ok) { throw new Error('Fetch missed, pls check API server'); }
+    let data = await responseFactory.json();
     this.id = data.id;
     this.url = data.url;
-    this.title = data.title;
-    this.original_title = data.original_title;
-    this.year = data.year;
-    this.date_published = data.date_published;
-    this.duration = data.duration;
-    this.description = data.description;
-    this.long_description = data.long_description;
-    this.avg_vote = data.avg_vote;
-    this.imdb_score = data.imdb_score;
-    this.votes = data.votes;
-    this.metascore = data.metascore;
     this.image_url = data.image_url;
-    this.actors = data.actors;
-    this.directors = data.directors;
-    this.writers = data.writers;
+    this.title = data.title;
     this.genres = data.genres;
-    this.countries = data.countries;
-    this.languages = data.languages;
+    this.date_published = data.date_published;
     this.rated = data.rated;
-    this.company = data.company;
-    debugger
+    this.imdb_score = data.imdb_score;
+    this.directors = data.directors;
+    this.actors = data.actors;
+    this.duration = data.duration;
+    this.countries = data.countries;
+    this.description = data.description;
+    this.worldwide_gross_income = data.worldwide_gross_income;
+    // debugger
     return this;
   }
 }
+/**
+ * Represents a light version of a movie to supply carousels.
+ * @constructor
+ * @param {number} id - The id of the movie within the API db..
+ * @param {uri} url - where the uri of the resource can be found.
+ * @param {string} title - The author of the movie.
+ * @param {number} imdb_score - The score collected by the site after visitor's votes.
+ * @param {uri} image_url - The image from the movie poster.
+ * @param {Array} genres - The genres the movie belongs to.
+ */
 
-function createMovie(movieUrl) {
+class MovieLight {
+
+  constructor(id, url, image_url, title, genres, imdb_score) {
+    this.id = id;
+    this.url = url;
+    this.image_url = image_url;
+    this.title = title;
+    this.genres = genres;
+    this.imdb_score = imdb_score;
+  }
+
+}
+
+let createFull = function createMovie(movieUrl) {
   let x = new Movie();
   return x.init(movieUrl);
 }
 
-let bestFilmUrl = "http://localhost:8000/api/v1/titles/1508669";
 
-createMovie(bestFilmUrl).then(function (movie) { console.log(movie) })
+
+let checkApiServer = async function () {
+  try {
+    let responseCheck = await fetch(baseUrl);
+    if (!responseCheck.ok) { throw new Error('Fetch missed, pls check API server'); }
+
+  } catch (error) {
+    throw console.error(error);;
+  }
+
+}
+let getSortedMovies = async function (sortedMoviePageUrl) {
+  let response = await fetch(sortedMoviePageUrl);
+
+  if (!response.ok) { throw new Error('Fetch missed, pls check API server'); }
+  let data = await response.json();
+  // console.log('data', data);
+  return await data
+}
+
+
+/**
+ * @function
+ * collects as many as set movies IDs for the listed genres 
+ * with async fetch while condition can't depend on promise, therefore:
+ * 6 genres + 1 best per 7 
+ * the OCMovies-API supplies 5 items per pages
+ * at least 2 pages will be loaded
+ * loop of parallel getters
+ */
+let retrieveSortedMovies = async function () {
+  for (let categoryCurrent of categorieList) {
+    moviesObject[categoryCurrent] = [];
+    let categoryLength = 0;
+    let searchUrl = baseUrl;
+    if (categoryCurrent != bestCategory) { searchUrl += "&genre=" + categoryCurrent; }
+    let maxLoop = 3;
+
+    while ((maxLoop > 1) && (searchUrl != null)) {
+      // console.log('searchUrl:', searchUrl);
+      getSortedMovies(searchUrl)
+        .then(async (sortedMovieList) => {
+          for await (movieCurrent of sortedMovieList.results) {
+            moviesObject[categoryCurrent].push(
+              new MovieLight(movieCurrent.id, movieCurrent.url, movieCurrent.image_url, movieCurrent.title, movieCurrent.genres, movieCurrent.imdb_score))
+            categoryLength++;
+          };
+          searchUrl = await sortedMovieList.next;
+        })
+      maxLoop--;
+    }
+  }
+}
+
+
+let baseUrl = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score";
+let bestFilmUrl = "http://localhost:8000/api/v1/titles/1508669";
+let bestCategory = 'Best'
+let categorieList = [bestCategory, 'Fantasy', 'Action', 'Documentary', 'Crime', 'Sci-Fi', 'Western'];
+let numberOfMoviesPerCategoryToShow = 7;
+/** @type {category:MovieLight[]}  [{'Fantasy': [MovieLight()...]}]*/
+let moviesObject = {}
+// createFull(bestFilmUrl).then(function (movie) { console.log(movie) })
+
+checkApiServer().then((sucess) => {
+  retrieveSortedMovies();
+  console.log('moviesObject:', moviesObject);
+})
+  .catch((reject) => {
+    throw ('The API server is not available, end of the App');
+  }
+
+  )
 
